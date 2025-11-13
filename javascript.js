9bfbaa50a062e1f9712d0694f0b60262da1eec3b
Michael Morris <michaelmorris121@yahoo.com>
2025-11-13 17:23:28 -0600

    create function to randomize color

--- javascript.js
+++ javascript.js
@@ -5,6 +5,13 @@ for (i = 0; i < 256; i++) {
     square.classList.add("squares");
     container.appendChild(square);
     square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "blue";
+        square.style.backgroundColor = randomColor();
     });
+ };
+
+ function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
  };
