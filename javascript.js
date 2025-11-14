fe71b84a68a2f6744b55905b508a30b5c29bcbcc
Michael Morris <michaelmorris121@yahoo.com>
2025-11-14 10:09:07 -0600

    create function to update squares
    so the amount of squares and size of squares
    change based on user input

--- javascript.js
+++ javascript.js
@@ -1,13 +1,23 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-for (i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("squares");
-    container.appendChild(square);
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = randomColor();
-    });
- };
+function grid(size) {
+    container.textContent = "";
+    const squareSize = 640 / size;
+
+    for (i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("squares");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        container.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = randomColor();
+        });
+    };
+};
+
+grid(16);
 
  function randomColor() {
     const r = Math.floor(Math.random() * 256);
